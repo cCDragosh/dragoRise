286eb53772c067f9ad7d65f5c4b722abf8e2eff6
cCDragosh <102684362+cCDragosh@users.noreply.github.com>
2025-05-04 21:16:45 +0200

    Delete generate_keypair.py
    delete

--- generate_keypair.py
+++
@@ -1,8 +0,0 @@
-from stellar_sdk import Keypair
-
-# Generate a new key pair
-pair = Keypair.random()
-
-# Print the secret and public keys
-print(f"Secret: {pair.secret}")
-print(f"Public Key: {pair.public_key}")
